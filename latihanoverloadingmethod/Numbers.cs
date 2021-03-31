c31f7d8f94adc8f2444276d3ed97a07efceb6bec
Adetia <adetiachrisnaputra@students.amikom.ac.id>
2021-03-31 22:01:23 +0700

    latihan overloading method

---
+++ latihanoverloadingmethod/Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanoverloadingmethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return (new int[] { number1, number2 }).Min();
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return (new int[] { number1, number2, (int)number3 }).Min();
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return (new int[] { number1, number2 }).Max();
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return (new int[] { number1, number2, (int)number3 }).Max();
+        }
+    }
+}
